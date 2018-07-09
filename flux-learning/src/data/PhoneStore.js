119688c810b359580986b0db1fd925bd303973c5
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-07-09 17:57:10 +0300

    Add store and view

---
+++ flux-learning/src/data/PhoneStore.js
@@ -0,0 +1,36 @@
+import Immutable from 'immutable';
+import { ReduceStore } from 'flux/utils';
+
+import ActionTypes from './ActionTypes.js';
+import PhonesDispatcher from './PhonesDispatcher.js';
+
+class PhoneStore extends ReduceStore {
+    constructor() {
+        super(PhonesDispatcher);
+    }
+
+    getInitialState() {
+        return Immutable.List.of('IPhone X', 'Google Pixel 3', 'Huawei P20 Pro');
+    }
+
+    reduce(state, action) {
+        switch (action.type) {
+            case ActionTypes.ADD_ITEM:
+                if (action.text) {
+                    return state.push(action.text);
+                }
+
+                return state;
+            case ActionTypes.REMOVE_ITEM:
+                if (~state.indexOf(action.text)) {
+                    return state.delete(index);
+                }
+
+                return state;
+            default:
+                return state;
+        }
+    }
+}
+
+export default new PhoneStore();
---
+++ flux-learning/src/views/AppView.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+class AppView extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { newItem: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ newItem: event.target.value });
+    }
+
+    onClick(event) {
+        if (!this.state.newItem) {
+            return;
+        }
+
+        this.state.props.onAddItem(this.state.newItem);
+        this.setState({ newItem: '' });
+    }
+
+    onRemove(item) {
+        if (!item) {
+            return;
+        }
+
+        this.props.onRemoveItem(item);
+    }
+
+    render() {
+        let remove = this.props.onRemoveItem;
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.newItem}
+                    onChange={this.onInputChange}
+                />
+                <button onClick={this.onClick}>Add</button>
+                <h2>
+                    Smartphones list
+                </h2>
+                <div>
+                    {
+                        this.props.phones.map(item => <Phone key={item} text={item} onRemove={remove} />)
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+class Phone extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { text: props.text };
+        this.onClick = tkis.onClick.bind(this);
+    }
+
+    onClick(event) {
+        this.props.onRemove(this.state.text);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>
+                    <b>
+                        {this.state.text}
+                    </b>
+                    <br />
+                    <button onClick={this.onClick}>Remove</button>
+                </p>
+            </div>
+        );
+    }
+}
+
+export default AppView;
