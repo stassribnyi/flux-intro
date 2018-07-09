e68e33600b21e99b8a95f6a6b1f542128bbd2444
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-07-09 18:12:10 +0300

    Add container and configuartions

---
+++ flux-learning/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+ 
+module.exports = {
+   entry: "./src/app.js", // входная точка - исходный файл
+   output:{
+       path: path.resolve(__dirname, './public'),     // путь к каталогу выходных файлов - папка public
+       publicPath: '/public/',
+       filename: "bundle.js"       // название создаваемого файла
+   },
+   module:{
+       rules:[   //загрузчик для js
+           {
+               test: /\.js?$/, // определяем тип файлов
+               exclude: /(node_modules)/,  // исключаем из обработки папку node_modules
+               loader: "babel-loader",   // определяем загрузчик
+               options:{
+                   presets:["env", "react"]    // используемые плагины
+               }
+           }
+       ]
+   }
+}
